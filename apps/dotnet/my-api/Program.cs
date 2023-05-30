f44ea5708430fdaedfef7cd46389f62d837e7288
jakub.pawlowski <jakub.pawlowski@guestline.com>
2023-05-30 09:13:35 +0200

    console

--- apps/dotnet/my-api/Program.cs
+++ apps/dotnet/my-api/Program.cs
@@ -1,6 +1,10 @@
+Console.WriteLine($"init...");
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+Console.WriteLine($"app.run...");
 
 app.Run();
