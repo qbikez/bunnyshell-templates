52b5e8c0e509ae562236c7bdbcb0f06478e1e838
jakub.pawlowski <jakub.pawlowski@guestline.com>
2023-05-30 05:53:07 +0200

    update terraform module

--- terraform/storage_account/storage_account.tf
+++ terraform/storage_account/storage_account.tf
@@ -24,3 +24,17 @@ resource "azurerm_storage_account" "main" {
   account_replication_type  = "LRS"
   tags                      = local.tags
 }
+
+
+
+resource "azurerm_storage_container" "oc-private" {
+  name                  = "oc-private"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "oc-public" {
+  name                  = "oc-public"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "blob"
+}
