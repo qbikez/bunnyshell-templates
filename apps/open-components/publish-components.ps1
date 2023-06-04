501ac0ba893b2a60a3dfbc235014b333abff8056
jakub.pawlowski <jakub.pawlowski@guestline.com>
2023-06-04 10:14:07 +0200

    debug

--- apps/open-components/publish-components.ps1
+++ apps/open-components/publish-components.ps1
@@ -1,11 +1,16 @@
 param()
 
+Set-PSDebug -Trace 1
+
 $components = Get-ChildItem "packages" | % { $_.Name.Replace(".tar.gz","") }
 foreach($component in $components) {
     write-host "publishing $component"
     $shouldPublish = .\check-ocversion.ps1 -ocUrl $env:OC_REGISTRY -ocName $component
     if ($shouldPublish) {
         npx oc publish --skipPackage $component --registries $env:OC_REGISTRY --username $env:OC_USERNAME --password $env:OC_PASSWORD
+        if ($LASTEXITCODE -ne 0) {
+            throw "failed to publish component $component"
+        }
     } else {
         write-host "version already exists. skipping."
     }
