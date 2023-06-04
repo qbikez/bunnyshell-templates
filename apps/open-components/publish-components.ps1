22c66aeb784ac91bf4aa562b807194e4e8d9e0d2
jakub.pawlowski <jakub.pawlowski@guestline.com>
2023-06-04 08:28:12 +0200

    fixyfix

--- apps/open-components/publish-components.ps1
+++ apps/open-components/publish-components.ps1
@@ -5,7 +5,7 @@ foreach($component in $components) {
     write-host "publishing $component"
     $shouldPublish = .\check-ocversion.ps1 -ocUrl $env:OC_REGISTRY -ocName $component
     if ($shouldPublish) {
-        npx oc publish "package/$component.tar.gz" --registries $env:OC_REGISTRY --username $env:OC_USERNAME --password $env:OC_PASSWORD
+        npx oc publish --skipPackage $component --registries $env:OC_REGISTRY --username $env:OC_USERNAME --password $env:OC_PASSWORD
     } else {
         write-host "version already exists. skipping."
     }
