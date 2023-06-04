ebd2d623c6614783654c5a51b660901740635027
jakub.pawlowski <jakub.pawlowski@guestline.com>
2023-06-04 13:41:26 +0200

    debug

--- apps/open-components/publish-components.ps1
+++ apps/open-components/publish-components.ps1
@@ -4,7 +4,7 @@ param()
 
 $components = Get-ChildItem "packages" | % { $_.Name.Replace(".tar.gz","") }
 foreach($component in $components) {
-    write-host "publishing $component to $env:OC_REGISTRY"
+    write-host "publishing $component to $($env:OC_USERNAME):$($env:OC_PASSWORD)@$env:OC_REGISTRY"
     $shouldPublish = .\check-ocversion.ps1 -ocUrl $env:OC_REGISTRY -ocName $component
     if ($shouldPublish) {
         npx oc publish --skipPackage $component --registries $env:OC_REGISTRY --username $env:OC_USERNAME --password $env:OC_PASSWORD
