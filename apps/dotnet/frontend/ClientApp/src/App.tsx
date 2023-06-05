28378744e614b2bd15e41344237d27ce171acb0c
jakub.pawlowski <jakub.pawlowski@guestline.com>
2023-06-05 06:29:03 +0200

    use api

--- apps/dotnet/frontend/ClientApp/src/App.tsx
+++ apps/dotnet/frontend/ClientApp/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [weather, setWeather] = useState<
+    Array<{
+      date: string;
+      temperatureC: number;
+      summary: string;
+    }>
+  >([]);
 
   return (
     <>
@@ -25,11 +32,28 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div>
+        <button
+          onClick={async () => {
+            const response = await fetch("api/WeatherForecast");
+            const data = await response.json();
+            setWeather(data);
+            console.log(data);
+          }}
+        >
+          weather?
+        </button>
+        <div>{weather.map((w) => (<div key={w.date}>
+          <span>{w.date}|</span>
+          <span>{w.temperatureC}|</span>
+          <span>{w.summary}</span>
+        </div>))}</div>
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
