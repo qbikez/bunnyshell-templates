8a823654ab4dd16917ac79e2baebe13b38afd0e1
jakub.pawlowski <jakub.pawlowski@guestline.com>
2023-06-09 06:08:12 +0200

    skip publish

--- apps/open-components/entrypoint.ps1
+++ apps/open-components/entrypoint.ps1
@@ -1,6 +1,6 @@
-./publish-components.ps1
+# ./publish-components.ps1
 
-write-host "publish done. starting infinite loop"
+write-host "publish SKIPPED. starting infinite loop"
 while($true) {
     Start-Sleep -Seconds 60
 }
