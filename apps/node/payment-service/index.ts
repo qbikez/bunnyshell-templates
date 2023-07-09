b72183bb5b5c536d3ceb579aa25a8c861033db0c
jakub.pawlowski <jakub.pawlowski@guestline.com>
2023-07-09 07:01:51 +0200

    test rest

--- apps/node/payment-service/index.ts
+++ apps/node/payment-service/index.ts
@@ -1,22 +1,37 @@
-import express from 'express';
+import express from "express";
+import bodyParser from "body-parser";
 
-const app = express()
-const port = 3000
+declare global {
+  namespace NodeJS {
+    interface ImportMeta {
+      hot: {};
+    }
+  }
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!!!!')
-})
+const port = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.send("hello from payment service");
+});
+
+app.post("/pay", (req, res) => {
+  const { orderId } = req.body;
+  console.log(`payment accepted for order ${orderId}`);
+  res.status(204).json({ message: "payment accepted" });
+});
 
 const server = app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Payments listening on port ${port}`);
+});
 
-if (import.meta.hot) {
-  import.meta.hot.on("vite:beforeFullReload", () => {
+if ((import.meta as any).hot) {
+  // need to manually kill old server before reloading
+  // https://github.com/vitest-dev/vitest/issues/2334
+  (import.meta as any).hot.on("vite:beforeFullReload", () => {
     server.close();
   });
-	// import.meta.hot.accept(() => {
-  //   console.log("hot relad callback!!");
-	// 	//await app.close();
-	// });
 }
